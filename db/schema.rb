4340a401ead281151bf584f7289200a37c4e458d
marusky <adrianmarusakza@gmail.com>
2023-09-08 12:30:59 +0200

    Clean redundant db tables (main)

--- db/schema.rb
+++ db/schema.rb
@@ -14,31 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_12_123713) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "classrooms", force: :cascade do |t|
-    t.string "name"
-    t.bigint "teacher_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["teacher_id"], name: "index_classrooms_on_teacher_id"
-  end
-
-  create_table "teachers", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["confirmation_token"], name: "index_teachers_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_teachers_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -67,5 +42,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_12_123713) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "classrooms", "teachers"
 end
